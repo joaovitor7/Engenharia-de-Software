9bf7e0eec0c6dafe76f5afcc7fbac84a69e91c0d
J.Vitor <vitorpeu@hotmail.com>
2016-07-12 21:12:02 -0300

    Conta e Sistema em Inglês

--- less work/main.py
+++ less work/main.py
@@ -26,6 +26,7 @@ from google.appengine.api import users
 class Usuario(db.Model):
     usuario=db.StringProperty(default='')
     senha=db.StringProperty(default='')
+    
 
 def UsuarioKey():
     return db.Key.from_path('Usuario','defaultusuario')
@@ -43,15 +44,15 @@ class LoginActivity(webapp2.RequestHandler):
                                         '<font face="verdana" color="green">'
                                         '<center><p><img src="https://4.bp.blogspot.com/-Mb01oiIvCmQ/V2apaVRDxHI/AAAAAAAAACw/InWzrXaW_cY8IyEuUXy6Ak3Dbp05dTnDwCLcB/s1600/less%2Bwork.jpg" alt="logo" height="95" width="360"></p><center>'
                                         '<form action="/Home" method="POST">'
-                                                            'Usuario:<input type=text name=usuario value=""/>'
-                                                            'Senha<input type="password" name=senha value="">'
+                                                            'User:<input type=text name=usuario value=""/>'
+                                                            'Password<input type="password" name=senha value="">'
                                                             '<br><button >Login</button>'
                                                             
 
                                         '</form>'
 
                                         #'<button onclick="myFunction()">Try it</button>'
-					'<p><a href="/AddUsuario">Sing in</a></p>'
+					'<p><a href="/AddUsuario">Sign up</a></p>'
 
                                         #'<p id="demo"></p>'
 
@@ -119,9 +120,9 @@ class AddUsuario(webapp2.RequestHandler):
                 '<body bgcolor="#B0F9EA">'
                 '<font face="verdana" color="green">'
 				'<form action="/AddUsuario" method="POST">'
-                    'Nome de usuario:<input type=text name=usuario value=""/>'
-                    '<br>Senha:<input type="password" name=senha value=""/>'
-                    '<br>Repita a senha:<input type="password" name=resenha value=""/>'
+                    'User:<input type=text name=usuario value=""/>'
+                    '<br>Password:<input type="password" name=senha value=""/>'
+                    '<br>Confirm password:<input type="password" name=resenha value=""/>'
                     '<br><input type=submit text="Salvar"/>'
                 '</form>'
                 '</body>'
@@ -145,15 +146,48 @@ class AddUsuario(webapp2.RequestHandler):
             
 class Home(webapp2.RequestHandler):
     def get(self):
-       self.response.write(
-            '<html>'
-                '<body>'
-                '<body bgcolor="#B0F9EA">'
-                '<font face="verdana" color="green">'
-                '<p><h1>Bem Vindo!</h1><p>'
-                '</body>'
-            '</html>'
-            )
+       self.response.write('<!DOCTYPE html>'
+                            '<html>'
+                            '<head>'
+                            '<title>Less Work</title>'
+                            '<meta name="viewport" content="width=device-width, initial-scale=1">'
+                            '<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">'
+                            '</head>'
+                            '<body>'
+                            '<body bgcolor="#B0F9EA">'
+                            '<font face="verdana" color="green">'
+                             '<h1>Welcome to LessWork</h1>'
+                            '<p>This is a system for  time control of job or study that think in a better time for work and a better time for you enjoy your life.</p>'
+                            #'<form action="/AddUsuario" method="POST">'
+                                                'You study or work?:<input type=text name=x value=""/>'
+                                                '<br>How many hours you work today?:<input type="number" name=y value="" id="hours day"/>'
+                                                '<br>What is the hours that you wake up?:<input type="number" name=z value=""/>'
+                                                '<br><button onclick="myFunction()">calculate</button>'
+                           
+                            #'<p>Calculating:</p>'
+                            '<i class="fa fa-spinner fa-spin" style="font-size:24px"></i>'
+                            '<p>Break hours:</p>'
+                            '<p id="demo"></p>'
+                            '<p id="para"></p>'
+                            '<a href="https://chrome.google.com/webstore/detail/pomodoro-timer/hfgjlgjnpkpmnpojkkpfkogapiclopop" target="_blank">Pomodoro timer</a>'
+                            '<script>'
+                            'function myFunction() {'
+                                'var x = document.getElementById("hours day").value;'
+                                'var minutos= 60*x;'
+                                'var z= Math.floor(minutos/100);'
+                                'var y= minutos%100;'
+                                'if (z>=25){'
+                                   'y=Math.floor(y/25)+5+(y%25);'
+                                '}'
+                                'var hd=(x*35+y)/60;'
+                                'document.getElementById("demo").innerHTML = hd;'
+                                'document.getElementById("para").innerHTML = "For a better result we recomended Podomoro timer. Link:";'
+                                
+                                
+                            '}'
+			    '</script>'
+                            '</body>'
+                            '</html>')
     def post(self):
         usuario = self.request.get('usuario')
         senha =self.request.get('senha')
